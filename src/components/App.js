905ce109bf86e1d76e48970d95eefc2410055c8d
Yuri Dogandjiev <yudogan@microsoft.com>
2018-03-08 00:00:31 -0800

    Initial check-in of React-Apollo tutorial

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import LinkList from './LinkList'
+import CreateLink from './CreateLink'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
+    )
+  }
+}
+
+export default App
