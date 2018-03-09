39053d9fd51a15dbe5eaea4132520b3492b8c49a
Yuri Dogandjiev <yudogan@microsoft.com>
2018-03-08 17:05:53 -0800

    Add pagination

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
+import { LINKS_PER_PAGE } from '../constants'
 import { FEED_QUERY } from './LinkList'
 
 class CreateLink extends Component {
@@ -38,15 +39,26 @@ class CreateLink extends Component {
     await this.props.postMutation({
       variables: {
         description,
-        url
+        url,
       },
       update: (store, { data: { post } }) => {
-        const data = store.readQuery({ query: FEED_QUERY })
+        const first = LINKS_PER_PAGE
+        const skip = 0
+        const orderBy = 'createdAt_DESC'
+        const data = store.readQuery({
+          query: FEED_QUERY,
+          variables: { first, skip, orderBy },
+        })
         data.feed.links.splice(0, 0, post)
-        store.writeQuery({ query: FEED_QUERY, data })
+        data.feed.links.pop()
+        store.writeQuery({
+          query: FEED_QUERY,
+          data,
+          variables: { first, skip, orderBy },
+        })
       },
     })
-    this.props.history.push('/')
+    this.props.history.push(`/new/1`)
   }
 }
 
